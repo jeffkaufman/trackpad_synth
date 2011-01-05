e252f579bf058847afbb5028443687d5acb0b298
Jeff Kaufman <jeff@adverplex.com>
2011-01-05 15:25:15 -0500

    kill program on window close

--- TrackpadSynth/TrackpadSynthAppDelegate.m
+++ TrackpadSynth/TrackpadSynthAppDelegate.m
@@ -15,4 +15,9 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
 	// Insert code here to initialize your application 
 }
+
+-(BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication*)theApplication
+{
+	return YES;
+}
 @end
